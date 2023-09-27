9c810dee600262fbbc2b3ab40dd47da87ecc8c1a
Quinn Klassen <klassenq@gmail.com>
2023-09-27 10:18:44 -0700

    worker auto tune

--- internal/client.go
+++ internal/client.go
@@ -489,21 +489,25 @@ type (
 		Authority string
 
 		// Enables keep alive ping from client to the server, which can help detect abruptly closed connections faster.
+		// Deprecated
 		EnableKeepAliveCheck bool
 
 		// After a duration of this time if the client doesn't see any activity it
 		// pings the server to see if the transport is still alive.
 		// If set below 10s, a minimum value of 10s will be used instead.
+		// Default: 30s
 		KeepAliveTime time.Duration
 
 		// After having pinged for keepalive check, the client waits for a duration
 		// of Timeout and if no activity is seen even after that the connection is
 		// closed.
+		// Default: 15s
 		KeepAliveTimeout time.Duration
 
 		// If true, client sends keepalive pings even with no active RPCs. If false,
 		// when there are no active RPCs, Time and Timeout will be ignored and no
 		// keepalive pings will be sent.
+		// Deprecated
 		KeepAlivePermitWithoutStream bool
 
 		// MaxPayloadSize is a number of bytes that gRPC would allow to travel to and from server. Defaults to 128 MB.
